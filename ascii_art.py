8b1de13eb49d2c5443e0024d1b56766ef76699e6
Iwan Saputra <ngonar@gmail.com>
2025-08-07 13:33:53 +1200

    ascii art sample

---
+++ ascii_art.py
@@ -0,0 +1,11 @@
+
+
+# Website : https://www.ascii-art.site/#ascii-text
+# FONT LIST : https://www.ascii-art.site/FontList.html
+
+
+from art import *
+art_1=art("coffee")
+print(art_1)
+
+tprint("Sam Sun", font="random")
